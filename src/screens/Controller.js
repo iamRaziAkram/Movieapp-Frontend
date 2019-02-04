0d3b1c1363309fb0cd5162567aa85259aeb23a39
iamRaziAkram <37903391+iamRaziAkram@users.noreply.github.com>
2019-02-04 14:03:40 +0530

    newIPO

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://54.166.91.112:8080/api/v1/";
+    this.baseUrl = "http://3.93.237.59:8080/api/v1/";
   }
   render(){
     return(

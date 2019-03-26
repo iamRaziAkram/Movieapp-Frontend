01936b65b7010e4d7aa4937d0e5fccb5f89022bb
iamRaziAkram <37903391+iamRaziAkram@users.noreply.github.com>
2019-03-26 10:21:18 +0530

    updated ip

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://54.89.183.141:8080/api/v1/";
+    this.baseUrl = "http://34.207.69.188:8085/api/v1/";
   }
   render(){
     return(
